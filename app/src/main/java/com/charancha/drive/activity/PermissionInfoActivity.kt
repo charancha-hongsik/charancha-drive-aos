5ac8a140715501167fe383ff7327edc992b8ccb2
hongsik <hongsik.jo@charancha.com>
2024-06-19 13:08:42 +0900

    로그인, 권한설정, 온보딩 UI 작업중

--- app/src/main/java/com/charancha/drive/activity/PermissionInfoActivity.kt
+++ app/src/main/java/com/charancha/drive/activity/PermissionInfoActivity.kt
@@ -1,13 +1,29 @@
 package com.charancha.drive.activity
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.charancha.drive.R
 
 class PermissionInfoActivity: AppCompatActivity(){
+    lateinit var layoutPermissionInfoConfirm:ConstraintLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_permission_info)
+
+        setResource()
+        setListener()
+    }
+
+    fun setResource(){
+        layoutPermissionInfoConfirm = findViewById(R.id.layout_permission_info_confirm)
+    }
+
+    fun setListener(){
+        layoutPermissionInfoConfirm.setOnClickListener {
+            startActivity(Intent(this@PermissionInfoActivity, PermissionActivity::class.java))
+        }
     }
 }
