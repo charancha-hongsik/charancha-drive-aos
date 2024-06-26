c151cf319a7dfb43dffdb61f8a8ec9e62b1136d4
hongsik <hongsik.jo@charancha.com>
2024-06-26 11:09:05 +0900

    차량등록 화면 UI 수정중

--- app/src/main/java/com/charancha/drive/activity/RegisterCarActivity.kt
+++ app/src/main/java/com/charancha/drive/activity/RegisterCarActivity.kt
@@ -2,6 +2,11 @@ package com.charancha.drive.activity
 
 import android.os.Bundle
 import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.charancha.drive.R
 
@@ -10,6 +15,9 @@ class RegisterCarActivity: AppCompatActivity() {
     lateinit var view_register_percent1: View
     lateinit var view_register_percent2:View
     lateinit var view_register_percent3:View
+    lateinit var ib_arrow_register_car:ImageButton
+    lateinit var tv_register_car_hint: TextView
+    lateinit var et_register_car: EditText
 
 
 
@@ -23,8 +31,6 @@ class RegisterCarActivity: AppCompatActivity() {
 
     fun init(){
         setResources()
-
-
     }
 
     fun setResources(){
@@ -32,5 +38,18 @@ class RegisterCarActivity: AppCompatActivity() {
         view_register_percent2 = findViewById(R.id.view_register_percent2)
         view_register_percent3 = findViewById(R.id.view_register_percent3)
 
+        ib_arrow_register_car = findViewById(R.id.ib_arrow_register_car)
+        tv_register_car_hint = findViewById(R.id.tv_register_car_hint)
+        et_register_car = findViewById(R.id.et_register_car)
+
+        view_register_percent1.isSelected = true
+        et_register_car.setOnFocusChangeListener { view, b ->
+            if(b){
+                tv_register_car_hint.visibility = GONE
+            }else{
+                tv_register_car_hint.visibility = VISIBLE
+
+            }
+        }
     }
 }
