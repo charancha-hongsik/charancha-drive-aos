9fcf8d93c7f81abe6b9cb47156e7fe44fb9ef34c
hongsik <hongsik.jo@charancha.com>
2024-07-09 10:49:58 +0900

    요약된 약관 목록 리팩토링 수정 건

--- app/src/main/java/com/charancha/drive/retrofit/response/TermDetailResponse.kt
+++ app/src/main/java/com/charancha/drive/retrofit/response/TermDetailResponse.kt
@@ -3,9 +3,11 @@ package com.charancha.drive.retrofit.response
 data class TermDetailResponse (
         val id:String,
         val createdAt:String,
+        val deletedAt:String,
+        val updatedAt:String,
         val title:String,
+        val version:String,
         val content:String,
-        val termsUsage:String,
-        val use:String,
-        val required:String
+        val isActive:String,
+        val isRequired:String
 )
