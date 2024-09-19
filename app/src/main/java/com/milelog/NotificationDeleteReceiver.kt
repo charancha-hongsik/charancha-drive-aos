6860e2e4671da098dc164ba1c17d0cc1e74a16cf
hongsik <hongsik.jo@charancha.com>
2024-09-19 09:36:22 +0900

    WOP-31 사용자가 권한을 해제했을 때, 주행관찰 프로세스 종료 안됨

--- app/src/main/java/com/milelog/NotificationDeleteReceiver.kt
+++ app/src/main/java/com/milelog/NotificationDeleteReceiver.kt
@@ -1,10 +1,17 @@
 package com.milelog
 
+import android.Manifest.permission.ACCESS_COARSE_LOCATION
+import android.Manifest.permission.ACCESS_FINE_LOCATION
+import android.Manifest.permission.ACTIVITY_RECOGNITION
 import android.app.ActivityManager
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.util.Log
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.milelog.service.BluetoothService
 
 class NotificationDeleteReceiver : BroadcastReceiver() {
@@ -13,9 +20,24 @@ class NotificationDeleteReceiver : BroadcastReceiver() {
     }    override fun onReceive(context: Context, intent: Intent) {
         // 여기에 알림이 삭제되었을 때의 작업을 작성합니다.
 
-        val bluetoothIntent = Intent(context, BluetoothService::class.java)
-        bluetoothIntent.action = ACTION_RESTART_NOTIFICATION
-        context.startForegroundService(bluetoothIntent)
+
+        if(ContextCompat.checkSelfPermission(context, ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(context, ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                if (ActivityCompat.checkSelfPermission(context, ACTIVITY_RECOGNITION) == PackageManager.PERMISSION_GRANTED) {
+                    if(!isMyServiceRunning(context, BluetoothService::class.java)){
+                        val bluetoothIntent = Intent(context, BluetoothService::class.java)
+                        bluetoothIntent.action = ACTION_RESTART_NOTIFICATION
+                        context.startForegroundService(bluetoothIntent)
+                    }
+                }
+            }else{
+                if(!isMyServiceRunning(context, BluetoothService::class.java)){
+                    val bluetoothIntent = Intent(context, BluetoothService::class.java)
+                    bluetoothIntent.action = ACTION_RESTART_NOTIFICATION
+                    context.startForegroundService(bluetoothIntent)
+                }
+            }
+        }
     }
 
     fun isMyServiceRunning(context:Context,serviceClass: Class<*>): Boolean {
