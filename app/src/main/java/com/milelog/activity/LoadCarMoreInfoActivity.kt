6caa680d793f0edcb5ff396998f4dce73a9631d7
hongsik <hongsik.jo@charancha.com>
2024-11-25 10:43:03 +0900

    기획서 확인중 1. 법인 또는 개인은 차량 등록 이후에    변경할 수 없어요. 등록할까요?

--- app/src/main/java/com/milelog/activity/LoadCarMoreInfoActivity.kt
+++ app/src/main/java/com/milelog/activity/LoadCarMoreInfoActivity.kt
@@ -293,7 +293,7 @@ class LoadCarMoreInfoActivity: BaseRefreshActivity() {
 
 
         btn_next.setOnClickListener {
-            CustomDialog(this, "차량 등록", "법인 또는 개인은 차량 등록 이후에\n변경할 수 없어요.", "등록","취소",  object : CustomDialog.DialogCallback{
+            CustomDialog(this, "차량 등록", "법인 또는 개인은 차량 등록 이후에\n변경할 수 없어요. 등록할까요?", "등록","취소",  object : CustomDialog.DialogCallback{
                 override fun onConfirm() {
                     var type:String = PERSONAL
                     var data:Data? = null
