c9c93968b924a5f4bedfec2f9d1a3387672ab205
hongsik <hongsik.jo@charancha.com>
2024-12-02 15:13:47 +0900

    QAI-1660 내 차 등록하기 > 사용자 구분 입력이 되지 않는 이슈

--- app/src/main/java/com/milelog/activity/LoadCarMoreInfoActivity.kt
+++ app/src/main/java/com/milelog/activity/LoadCarMoreInfoActivity.kt
@@ -633,7 +633,7 @@ class LoadCarMoreInfoActivity: BaseRefreshActivity() {
                     layout_select.visibility = GONE
 
 
-                    if(postMyCarResponse.type.equals(CORPORATE)){
+                    if(iv_corp.isSelected){
                         if(!et_corp_name.text.toString().isNullOrEmpty() && !et_corp_name.text.toString().isNullOrEmpty() && !et_corp_department.text.toString().isNullOrEmpty() && !postMyCarResponse.makerCd.isNullOrEmpty() && !postMyCarResponse.modelCd.isNullOrEmpty()){
                             btn_next.isSelected = true
                             btn_next.isClickable = true
