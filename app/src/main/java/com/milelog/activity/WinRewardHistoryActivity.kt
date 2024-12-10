8fc00079a55819fe197dfbf32cd7c4235ffebbee
hongsik <hongsik.jo@charancha.com>
2024-12-11 07:47:20 +0900

    당첨내역 UI

---
+++ app/src/main/java/com/milelog/activity/WinRewardHistoryActivity.kt
@@ -0,0 +1,12 @@
+package com.milelog.activity
+
+import android.os.Bundle
+import com.milelog.R
+
+class WinRewardHistoryActivity:BaseRefreshActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_win_reward)
+
+    }
+}
